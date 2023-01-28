38f27d3f421e510c009cf76d7f89dca1ff4e564d
amarbhore4141 <meetamarbhore@gmail.com>
2023-01-29 03:37:41 +0530

    hibernate with two xml files
    configuration
    and hbm files created

--- hibernateExampleXml/src/com/exponent/hibernateExampleXml/test.java
+++ hibernateExampleXml/src/com/exponent/hibernateExampleXml/test.java
@@ -1,5 +1,20 @@
 package com.exponent.hibernateExampleXml;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
-public class test {
+public class Test {
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+		SessionFactory sf = cfg.buildSessionFactory();
 
+		Student student = new Student();
+		student.setId(1);
+		student.setsName("Ramanujan");
+		student.setAddress("Tiruanantpuram");
+
+		Session session = sf.openSession();
+		session.save(student);
+		session.beginTransaction().commit();
+	}
 }
