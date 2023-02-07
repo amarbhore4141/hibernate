31e3c1bdbf927c5f0703b0bd955218d15c813f37
amarbhore4141 <meetamarbhore@gmail.com>
2023-02-07 11:50:21 +0530

    hibernate with log files

--- hibernateExampleXml/src/com/exponent/hibernateExampleXml/Student.java
+++ hibernateExampleXml/src/com/exponent/hibernateExampleXml/Student.java
@@ -1,30 +1,37 @@
 package com.exponent.hibernateExampleXml;
 
 public class Student {
-int id;
-String sName;
-String address;
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public String getsName() {
-	return sName;
-}
-public void setsName(String sName) {
-	this.sName = sName;
-}
-public String getAddress() {
-	return address;
-}
-public void setAddress(String address) {
-	this.address = address;
-}
-@Override
-public String toString() {
-	return "Student [id=" + id + ", sName=" + sName + ", address=" + address + "]";
-}
+	int id;
+	String sName;
+	String address;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getsName() {
+		return sName;
+	}
+
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", sName=" + sName + ", address=" + address + "]";
+	}
 
 }
--- hibernateExampleXml/src/com/exponent/hibernateExampleXml/test.java
+++ hibernateExampleXml/src/com/exponent/hibernateExampleXml/test.java
@@ -2,34 +2,46 @@ package com.exponent.hibernateExampleXml;
 
 import java.util.Scanner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class Test {
+
+	public static final Logger LOGGER = LogManager.getLogger(Test.class);
+
 	public static void main(String[] args) {
+		LOGGER.info("Main method Start....!");
 		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
 		SessionFactory sf = cfg.buildSessionFactory();
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("How many users do you want to add?");
+		LOGGER.info("How many users do you want to add?");
 		int totalUsers = sc.nextInt();
 
 		for (int i = 0; i < totalUsers; i++) {
 			Student student = new Student();
-			System.out.println("enter student ID");
+			//System.out.println("enter student ID");
+			LOGGER.info("enter student ID");
 			student.setId(sc.nextInt());
-			
-			System.out.println("Enter student name");
+
+			//System.out.println("Enter student name");
+			LOGGER.info("Enter student name");
 			student.setsName(sc.next());
-			
-			System.out.println("Enter student address");
+
+			//System.out.println("Enter student address");
+			LOGGER.info("Enter student address");
 			student.setAddress(sc.next());
 
-			Session session = sf.openSession();
-			session.save(student);
-			session.beginTransaction().commit();
+		Session session = sf.openSession();
+		session.save(student);
+		session.beginTransaction().commit();
 		}
-		System.out.println("users added successfully");
+		//System.out.println("users added successfully");
+		LOGGER.debug("users added successfully");
+		sc.close();
 	}
 }
